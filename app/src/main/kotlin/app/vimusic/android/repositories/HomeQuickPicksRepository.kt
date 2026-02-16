b7f2a763de70decafe31eb7625f013ce210fb0e4
haturatu <taro@eyes4you.org>
2026-02-17 01:33:42 +0900

    ref: move quick picks cache handling from screen to viewmodel/repository

--- app/src/main/kotlin/app/vimusic/android/repositories/HomeQuickPicksRepository.kt
+++ app/src/main/kotlin/app/vimusic/android/repositories/HomeQuickPicksRepository.kt
@@ -2,6 +2,7 @@ package app.vimusic.android.repositories
 
 import app.vimusic.android.Database
 import app.vimusic.android.models.Song
+import app.vimusic.android.preferences.DataPreferences
 import app.vimusic.android.query
 import app.vimusic.providers.innertube.Innertube
 import app.vimusic.providers.innertube.models.bodies.NextBody
@@ -14,6 +15,9 @@ interface HomeQuickPicksRepository {
     fun observeLastInteractionSong(): Flow<Song?>
     suspend fun fetchRelatedPage(videoId: String): Result<Innertube.RelatedPage?>?
     fun clearEventsFor(songId: String)
+    fun getCachedQuickPicksIfAvailable(): Innertube.RelatedPage?
+    fun cacheQuickPicks(page: Innertube.RelatedPage)
+    fun clearCachedQuickPicks()
 }
 
 object DatabaseHomeQuickPicksRepository : HomeQuickPicksRepository {
@@ -28,5 +32,24 @@ object DatabaseHomeQuickPicksRepository : HomeQuickPicksRepository {
 
     override fun clearEventsFor(songId: String) {
         query { Database.clearEventsFor(songId) }
+    }
+
+    override fun getCachedQuickPicksIfAvailable(): Innertube.RelatedPage? {
+        if (!DataPreferences.shouldCacheQuickPicks) return null
+        val cached = DataPreferences.cachedQuickPicks
+        return if (
+            cached.albums.isNullOrEmpty() &&
+            cached.artists.isNullOrEmpty() &&
+            cached.playlists.isNullOrEmpty() &&
+            cached.songs.isNullOrEmpty()
+        ) null else cached
+    }
+
+    override fun cacheQuickPicks(page: Innertube.RelatedPage) {
+        DataPreferences.cachedQuickPicks = page
+    }
+
+    override fun clearCachedQuickPicks() {
+        DataPreferences.cachedQuickPicks = Innertube.RelatedPage()
     }
 }
